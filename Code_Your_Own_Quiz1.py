a47d8d643504a5e8d3b4daf2756a41835e4724bc
P-Marlowe <P-Marlowe@users.noreply.github.com>
2016-05-13 11:31:28 -0400

    Updated take_quiz function
    Revised prompt to user and added a quiz string and a blanks_list list.

--- Code_Your_Own_Quiz1.py
+++ Code_Your_Own_Quiz1.py
@@ -1,3 +1,13 @@
+quiz = """A ___(1)___ is created with the def keyword. You specify the inputs a ___(1)___ takes by
+adding ___(2)___! separated by commas between the parentheses. ___(1)___, by default return None if you
+don't specify the value to return. Note that ___(2)___ can be standard data types such as string, number, dictionary,
+tuple, and list or can be more complicated such as objects and lambda functions."""
+
+
+
+blanks_list = ["___(1)___", "___(2)___", "___(3)___", "___(4)___", "___(5)___", "___(6)___"]
+
+
 def blanks_in_text(text, blanks_list):
     #function locates blanks in quiz text
     for blank in blanks_list:
@@ -12,7 +22,7 @@ def take_quiz(quiz_string, blanks_list):
     for text in quiz_string:
         replacement = blanks_in_text(text, blanks_list)
         if replacement != None:
-            user_input = raw_input("The answer is?")
+            user_input = raw_input("What is the answer to" + replacement +"?")
             text = text.replace(replacement, user_input)
             replaced.append(text)
         else:
