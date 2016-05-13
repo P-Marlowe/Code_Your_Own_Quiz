efe0b12c9c3fffba6077067fb7b24b65f7963f54
P-Marlowe <P-Marlowe@users.noreply.github.com>
2016-05-13 11:02:58 -0400

    Added Replacement Function

--- Code_Your_Own_Quiz1.py
+++ Code_Your_Own_Quiz1.py
@@ -4,3 +4,20 @@ def blanks_in_text(text, blanks_list):
         if blank in text:
             return blank
     return None
+
+def take_quiz(quiz_string, blanks_list):
+    #In current iteration, this function replaces the blanks
+    replaced = []
+    quiz_string = quiz_string.split()
+    for text in quiz_string:
+        replacement = blanks_in_text(text, blanks_list)
+        if replacement != None:
+            text = text.replace(replacement, "corgi")
+            replaced.append(text)
+        else:
+            replaced.append(text)
+    replaced = " ".join(replaced)
+    return replaced 
+                    
+    
+print take_quiz(quiz, blanks_list)
